94b358f58804885785b8c9694b1e6d9138ec342f
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-05 02:17:54 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+
+attr_accessor :name
+  
+@@all = []
+
+  def initialize(name)
+  @name = name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all = []
+  end
+
+end
